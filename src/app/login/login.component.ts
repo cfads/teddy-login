05cd9735689ff33d15923a9b109b00bb9b31f5bd
Cris <cristiansouza.agra@gmail.com>
2024-09-01 21:28:44 -0300

    Redirecionando aplicação angular para o app react

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,8 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatCheckboxChange } from '@angular/material/checkbox';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {}
+export class LoginComponent implements OnInit {
+  manterConectado: boolean = false;
+
+  ngOnInit(): void {
+    // const manterConectado = localStorage.getItem('manterConectado');
+    // if (manterConectado === 'true') {
+    //   window.location.href = 'https://teddy-app-flax.vercel.app/';
+    // }
+  }
+
+  onCheckboxChange(event: MatCheckboxChange): void {
+    this.manterConectado = event.checked;
+  }
+
+  login() {
+    localStorage.setItem(
+      'manterConectado',
+      this.manterConectado ? 'true' : 'false'
+    );
+    window.location.href = 'https://teddy-app-flax.vercel.app/';
+  }
+}
